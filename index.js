074f6b6878538ea708afdddb4ec574239db0864b
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-13 21:40:36 -0800

    Can use API to search for movies

---
+++ index.js
@@ -0,0 +1,18 @@
+const KEY = "74a8deec";
+const URL = "http://www.omdbapi.com/";
+
+const searchInput = document.getElementById("search");
+const searchForm = document.getElementById("search-form");
+const searchBtn = document.getElementById("search-btn");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchMovieResults();
+});
+
+async function fetchMovieResults() {
+  const title = searchInput.value;
+  const response = await fetch(`${URL}?apikey=${KEY}&s=${title}`);
+  const data = await response.json();
+  console.log(data);
+}
